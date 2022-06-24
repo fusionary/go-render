9f1e61af6922569c13f67035917b45ef0c9b7d6a
Bob VanLonkhuyzen <hey.bob.oh@gmail.com>
2022-06-24 11:17:19 -0400

    Add package file for jobs, func for creating job

---
+++ jobs.go
@@ -0,0 +1,31 @@
+package render
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func CreateJob(ctx context.Context, serviceId string) bool {
+	url := fmt.Sprintf("https://api.render.com/v1/services/%v/jobs", serviceId)
+
+	payload := strings.NewReader("{\"startCommand\":\"node index.js\"}")
+
+	req, _ := http.NewRequest("POST", url, payload)
+
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %v", ctx.Value("token")))
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	// body, _ := ioutil.ReadAll(res.Body)
+
+	if res.StatusCode == 200 {
+		return true
+	} else {
+		return false
+	}
+}
