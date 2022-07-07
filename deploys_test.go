b3fdec40ef5dee7792c318dbe4d6130bebf2dddf
JP Grusling <jp.grusling@fusionary.com>
2022-07-07 10:02:11 -0400

    update deploys on new structure

--- deploys_test.go
+++ deploys_test.go
@@ -2,6 +2,9 @@ package render
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"log"
 	"os"
 	"testing"
 
@@ -10,14 +13,22 @@ import (
 
 func TestTriggerDeploy(t *testing.T) {
 	godotenv.Load(".env")
-
 	token := os.Getenv("TOKEN")
 
 	ctx := context.Background()
-	ctx = StoreAuthentication(ctx, token)
 
-	got := TriggerDeploy(ctx, os.Getenv("DEPLOY_TEST_SERVICEID"))
-	if got == false {
-		t.Errorf("TriggerDeploy %v != true", got)
+	body := DeployTriggerBody{ClearCache: DoNotClear}
+	client := NewClient(nil, token)
+	data, res, err := client.Deploys.TriggerADeployment(ctx, os.Getenv("DEPLOY_TEST_SERVICEID"), body)
+	if err != nil {
+		fmt.Printf("Error while getting results")
+		fmt.Println(err)
+	} else {
+		json, err := json.Marshal(*data)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		log.Printf("%d %s", res.StatusCode, json)
 	}
 }
